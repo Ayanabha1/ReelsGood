cf38c0e197842a2c537f11db7937c0fd747588d6
Ayanabha Misra <misrarimbo@gmail.com>
2024-04-29 00:51:54 +0530

    added some console logs to track error in server logs

--- apps/admin/app/api/getMovies/route.ts
+++ apps/admin/app/api/getMovies/route.ts
@@ -3,9 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
+    console.log(req.url);
     const { searchParams } = new URL(req.url);
     const take: string = searchParams.get("take")!;
     const skip: string = searchParams.get("skip")!;
+    console.log(take);
+    console.log(skip);
     let movies;
     if (take && skip) {
       movies = await prismaDB.movie.findMany({
@@ -16,12 +19,14 @@ export async function GET(req: Request) {
       movies = await prismaDB.movie.findMany({});
     }
     const total = await prismaDB.movie.count();
+    console.log(movies);
     return NextResponse.json({
       movies: movies,
       total: total,
       message: "success",
     });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { message: "Could not fetch users" },
       { status: 400 }
